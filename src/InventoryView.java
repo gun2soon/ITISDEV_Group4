6958948f23b4ba952ddc33e964a6909d68ea7f4c
ylla14 <salgadoallyssa@gmail.com>
2024-07-15 14:08:43 +0800

    ud

--- src/InventoryView.java
+++ src/InventoryView.java
@@ -64,4 +64,12 @@ public class InventoryView {
     public JButton getExitButton() {
         return exitButton;
     }
+
+    public String getSelectedSKU() {
+        int selectedRow = table.getSelectedRow();
+        if (selectedRow != -1) {
+            return table.getValueAt(selectedRow, 2).toString();
+        }
+        return null;
+    }
 }
